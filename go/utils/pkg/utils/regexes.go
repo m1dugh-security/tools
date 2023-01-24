c8817b4a746ef6fde30e308cb69f5c4c990dcd15
m1dugh <romain103paris@gmail.com>
2023-01-24 17:39:47 +0100

    adds common regexes to package

---
+++ go/utils/pkg/utils/regexes.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+    "regexp"
+)
+
+const (
+    param string = `(\?[\-\w=\.~\;\[\]&]+)?`
+    localUrl = `(/[\w\.~=\-]+)+/?`
+    subdomain = `([\w\-]+\.)+[a-z]{2,7}`
+)
+
+var (
+    SubdomainMatcher = regexp.MustCompile(subdomain)
+    URLMatcher = regexp.MustCompile(`https?://`+ subdomain + localUrl + param)
+    EmailMatcher = regexp.MustCompile(`[\w\-\.]+@` + subdomain)
+)
